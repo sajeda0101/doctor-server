504e7fcafad51757722b94bceedf5f1fda35add9
unknown <sajeda1605akter@gmail.com>
2022-11-07 17:43:03 -0800

    data get for client side

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 require('dotenv').config();
 const cors=require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.PORT || 5000;
 
 // middleware
@@ -13,6 +14,39 @@ app.get('/',(req,res)=>{
     res.send('life moment')
 
 })
+// 
+// 
+
+
+const uri = "mongodb+srv://LifeCareUser:jNBQ2MwxNlFK9KIA@cluster0.dbebnio.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    
+    try{
+
+        const serviceCollection=client.db('lifecare').collection('services')
+
+        app.get('/services',async (req,res)=>{
+            const query={}
+            const cursor=serviceCollection.find(query)
+            const services= await cursor.toArray()
+            res.send(services)
+        })
+    }
+
+    finally{
+
+    }
+
+}
+run().catch(err=>console.log(err))
+
+
+
+
+
+
 
 app.listen(port,()=>{
     console.log('lifeemonent server running ',port)
